9f416980e2348b776b1f79047e57c6a3c724573a
John Lennon <johndoe@example.com>
2024-07-30 21:28:03 +0300

    dz ready

--- PySpark/lesson4/seminar4_DZ.py
+++ PySpark/lesson4/seminar4_DZ.py
@@ -1,5 +1,11 @@
+# Условие: используйте источник rate, напишите код, 
+# который создаст дополнительный столбец, 
+# который будет выводить сумму только нечётных чисел.
+
+
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as f
+from pyspark.sql.functions import col, sum
 
 # create spark session
 spark = SparkSession.builder.appName("FilterOddNumbersSum").getOrCreate()
@@ -7,6 +13,6 @@ spark = SparkSession.builder.appName("FilterOddNumbersSum").getOrCreate()
 df = spark.readStream.format("rate").load()
 # filter odds and add sum column
 df_even = df.filter("value % 2 == 1").agg(f.sum('value'))
-query = df_even.writeStream.outputMode("complete").format("console").start()
+query = df_even.writeStream.outputMode("update").format("console").start()
 query.awaitTermination()
 # spark.stop()
